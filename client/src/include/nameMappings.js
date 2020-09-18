84c98966c1928b3168384c3c68edd84a1a0311a1
Miles Watson <miles@milescwatson.com>
2020-09-18 16:34:52 -0700

    name mappings

---
+++ client/src/include/nameMappings.js
@@ -0,0 +1,22 @@
+
+var nameMappings = {
+  propertyValue: "Property Value",
+  grossRentalIncome: "Gross Rental Income (M)",
+  netOperatingExpenses: "Net Operating Expenses (M)",
+  netOperatingIncome: "Net Operating Income (M)",
+  paymentsAnnualized: "Mortgage Payment (M)",
+  cashFlow: "Cashflow (M)",
+  depreciation: "Depreciation (A)",
+  cashFlowIRS: "Cash Flow IRS (A)",
+  resultingTaxWriteoff: "Resulting Tax Writeoff (A)",
+  valueOfRealEstateInvestment: "Value of Real Estate Investment",
+  valueOfRealEstateInvestmentIncludingWriteoffs: "Value of Real Estate Investment Incl. Writeoffs",
+  valueOfStockMarketInvestment: "Value of Stock Market Investment",
+  remainingBalance: "Loan Balance Remaining",
+  totalEquity: "Total Equity",
+  annualInterest: "Annual Interest",
+  cumPrincipal: "Cumulative Principal",
+  cumInterest: "Cumulative Interest"
+ }
+
+exports.nameMappings = nameMappings;
